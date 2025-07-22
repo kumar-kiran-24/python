dbce7148d705eec3ae2bbab442787edf9e0e19e5
kumar-kiran-24 <kirankumar26650@gmail.com>
2025-07-22 20:25:28 +0530

    array shape

--- num_py/array_numpy.py
+++ num_py/array_numpy.py
@@ -8,5 +8,18 @@ arr3=np.array([1.1,2.3,4.5])
 arr4=arr3.astype(int)
 print(arr4)# change the dat type (arr4=1,2,3,4)
 
+#viem and copy
+
+arr5=arr.copy()
+arr[1][0]=88
+print("copy",arr5)
+print(arr)
 
 
+arr7 = np.array([[[[1,2,3],[1,2,3],[5,6,7]]]])
+
+#shape
+print(arr7.shape) #return (dienssions,lastly_give_the_elments)
+
+arr8=np.array([1,2,3,4],ndmin=5)#create the required numbers of dimmesional array using the nadmin
+print(arr8.shape)#(1,1,1,1,4)
--- num_py/class.py
+++ num_py/class.py
@@ -1,4 +1,14 @@
 class k:
-    x=5
-p=k()
-print(p.x)    
+    def __init__(s,fn,db):
+        s.fn=fn
+        s.db=db
+    def print(s):
+        print(s.fn,"\n",s.db)
+
+class b(k):
+    def __init__(s, fn, db):
+        super().__init__(fn, db)
+        print("hello")
+a=k("kian","29-11-2004")
+a.print()
+a.print()
